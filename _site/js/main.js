ba67827cd7016c0cb54cc58916a89f8ffc6cf272
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2018-01-05 18:31:35 +0700

    change service package, redo buttons and hover anims
    - restyled service packages, incl js for "read more" animations
    - fixed button hover animations.
    - tweaked header to make animations work

--- _site/js/main.js
+++ _site/js/main.js
@@ -13,3 +13,24 @@
 //     });
 // }
 // window.onload = init();
+$(".open").click( function () {
+  var container = $(this).parents(".package");
+  var packageDescription = container.find(".packageDescription");
+  var trigger = container.find(".faq-t");
+
+  packageDescription.slideToggle(200);
+
+  if (trigger.hasClass("faq-o")) {
+    trigger.removeClass("faq-o");
+  }
+  else {
+    trigger.addClass("faq-o");
+  }
+
+  if (container.hasClass("expanded")) {
+    container.removeClass("expanded");
+  }
+  else {
+    container.addClass("expanded");
+  }
+});
